522811b8bc892a402ab097f3cd107c03f0686e9b
Artur Miller <amiller89@gmx.de>
2019-02-24 19:30:51 +0100

    Fixed conversion to unique_ptr problem

--- src/py_contact_sensor.cpp
+++ src/py_contact_sensor.cpp
@@ -5,6 +5,6 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(py_contact_sensor, m) {
-    py::class_<gazebo::sensors::ContactSensor>(m, "PyContactSensor");
+    py::class_<gazebo::sensors::ContactSensor, std::shared_ptr<gazebo::sensors::ContactSensor>>(m, "PyContactSensor");
 }
 
